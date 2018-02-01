f0424a65118efec19492c7f64071dd890c2f9348
Meninblack007 <sanyam.53jain@gmail.com>
2018-02-01 09:35:43 +0000

    Telephony: Break MSIM dependency on qti-telephony-framework

--- src/com/android/phone/NotificationMgr.java
+++ src/com/android/phone/NotificationMgr.java
@@ -708,8 +708,7 @@ public class NotificationMgr {
 
     private boolean isUiccCardProvisioned(int subId) {
         final int PROVISIONED = 1;
-        final int INVALID_STATE = -1;
-        int provisionStatus = INVALID_STATE;
+        int provisionStatus = PROVISIONED;
         IExtTelephony mExtTelephony = IExtTelephony.Stub
                 .asInterface(ServiceManager.getService("extphone"));
         if (mExtTelephony != null) {
@@ -718,10 +717,8 @@ public class NotificationMgr {
                 //get current provision state of the SIM.
                 provisionStatus = mExtTelephony.getCurrentUiccCardProvisioningStatus(slotId);
             } catch (RemoteException ex) {
-                provisionStatus = INVALID_STATE;
                 if (DBG) log("Failed to get status for slotId: "+ slotId +" Exception: " + ex);
             } catch (NullPointerException ex) {
-                provisionStatus = INVALID_STATE;
                 if (DBG) log("Failed to get status for slotId: "+ slotId +" Exception: " + ex);
             }
         }
